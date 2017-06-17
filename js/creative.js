0267b2675ef285cf15b4e791baa005ca34052852
youngwookdo <YoungwookDo@DoYoungwooks-iMac.local>
2017-06-17 12:50:33 -0400

    trial
    trial

--- js/creative.js
+++ js/creative.js
@@ -60,23 +60,5 @@
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
         }
     });
-    
-    
-    // dropdown slide up & down
-    $('.dropdown').on('show.bs.dropdown', function(e){
-      var $dropdown = $(this).find('.dropdown-menu');
-      var orig_margin_top = parseInt($dropdown.css('margin-top'));
-      $dropdown.css({'margin-top': (orig_margin_top + 10) + 'px', opacity: 0}).animate({'margin-top': orig_margin_top + 'px', opacity: 1}, 10, function(){
-         $(this).css({'margin-top':''});
-      });
-   });
-   // Add slidedown & fadeout animation to dropdown
-   $('.dropdown').on('hide.bs.dropdown', function(e){
-      var $dropdown = $(this).find('.dropdown-menu');
-      var orig_margin_top = parseInt($dropdown.css('margin-top'));
-      $dropdown.css({'margin-top': orig_margin_top + 'px', opacity: 1, display: 'block'}).animate({'margin-top': (orig_margin_top + 10) + 'px', opacity: 0}, 10, function(){
-         $(this).css({'margin-top':'', display:''});
-      });
-   });
 
 })(jQuery); // End of use strict
